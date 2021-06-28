f45bc4980df1d014fbf5360ed488d2c1c5058007
huytunguyenn <49774218+huytunguyenn@users.noreply.github.com>
2021-06-28 18:46:03 +0700

    add cors

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,8 +1,137 @@
 const express = require("express");
 const router = express.Router();
 
+const StaffData = [
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+  {
+    id: "001",
+    name: "Hà Minh Cường",
+    adress: "Dormitory, VNU",
+    position: "CEO",
+    telephone: "0967968666",
+    department: "Administrator",
+    salary: "10.000.000",
+  },
+];
+
 router.get("/", (req, res, next) => {
-  res.send("anc");
+  res.send(StaffData);
 });
 
 module.exports = router;
