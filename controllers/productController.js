ea9d3cc6001c1459529fe07a2db0b3a392778018
huytunguyenn <49774218+huytunguyenn@users.noreply.github.com>
2021-06-30 10:35:14 +0700

    delete old data

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const productModel = require("../models/productModel");
-const product = require("../model/product.model")
-exports.detail = async(req, res, next) => {
-    listProduct = await product.getAllProduct();
-    console.log(listProduct)
-    res.send(listProduct);
+const product = require("../models/product.model");
+exports.detail = async (req, res, next) => {
+  listProduct = await product.getAllProduct();
+  console.log(listProduct);
+  res.send(listProduct);
 };
